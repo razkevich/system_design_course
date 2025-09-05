cadf239a22b21838529ead3590aeb2683c40e6cf
razkevich <razkevich8@gmail.com>
2025-09-05 14:35:34 +0300

    cleanup

--- moodle-local/moodledata/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'M6tx3hqpb8JbZR8EuBwLHPKHUOeAT8vvlocalhost';
-$CFG->bootstraphash = '3dc7f211d88c7c8eab70ff38f9e28dc5';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
